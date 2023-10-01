c8429383ba053c17ca62836d797c0215cccd9332
DESKTOP-AUNIITC\anhdu <duha2001@github.com>
2023-10-01 15:56:14 +0700

    config code QR code

--- Trello-Client/vite.config.js
+++ Trello-Client/vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
+import { qrcode } from 'vite-plugin-qrcode';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
-  // base: './'
+  plugins: [react(), svgr(), qrcode()],
+  base: './',
   resolve: {
     alias: [{ find: '~', replacement: '/src' }],
+  },
+  server: {
+    host: true,
   },
 });
